f7dc62e6b032fe145392fc8a0447a487d9c8eda8
Rajiv Jain <rajivpjs@gmail.com>
2018-01-17 21:00:06 +0000

    Add CountDownLatch as a counter to avoid synchronized block

---
+++ threads/src/main/java/com/wp/wpdp/ThreadSafety.java
@@ -0,0 +1,46 @@
+package com.wp.wpdp;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class CountDownProcessor implements Runnable {
+
+    private CountDownLatch latch;
+
+    public CountDownProcessor(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public void run() {
+        System.out.println("Started.");
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        latch.countDown();
+    }
+}
+
+public class ThreadSafety {
+
+    public static void main(String[] args) {
+        CountDownLatch latch = new CountDownLatch(3);
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        for(int i = 0; i < 3; i++) {
+            executor.submit(new CountDownProcessor(latch));
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Completed.");
+    }
+}
